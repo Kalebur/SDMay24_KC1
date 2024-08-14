799614090ef389214f6882e72328ee50c57badf2
James Smith <kaleburdev@gmail.com>
2024-08-14 18:29:55 -0400

    Add test for returning the sum of a collection of numbers

--- CalculatorTests/CalculatorOperationTests.cs
+++ CalculatorTests/CalculatorOperationTests.cs
@@ -6,6 +6,8 @@ namespace CalculatorTests
     public class CalculatorOperationTests
     {
         private static readonly Random _random = new();
+        private static readonly int _minInteger = -10_000;
+        private static readonly int _maxInteger = 10_000;
 
         [TestCase(null)]
         public static void Add_ThrowsNullException_IfCollectionIsNull(IEnumerable<int> input)
@@ -32,6 +34,24 @@ namespace CalculatorTests
             Assert.That(result, Is.EqualTo(firstItem));
         }
 
+        [TestCaseSource(nameof(GetCollectionOfIntegers))]
+        [TestCaseSource(nameof(GetCollectionOfIntegers))]
+        [TestCaseSource(nameof(GetCollectionOfIntegers))]
+        [TestCaseSource(nameof(GetCollectionOfIntegers))]
+        [TestCaseSource(nameof(GetCollectionOfIntegers))]
+        public static void Add_ReturnsSumOfAllNumbers_WhenGivenCollectionOfIntegers(IEnumerable<int> input)
+        {
+            BigInteger sum = 0;
+            foreach (var number in input)
+            {
+                sum += number;
+            }
+
+            var result = Calculator.Add(input);
+
+            Assert.That(result, Is.EqualTo(sum));
+        }
+
         private static object[] GetEmptyCollections()
         {
             return
@@ -43,9 +63,29 @@ namespace CalculatorTests
 
         private static IEnumerable<int>[] GetSingleValueCollection()
         {
-            var randomNumber = _random.Next(-10_000, 10_000);
+            var randomNumber = _random.Next(_minInteger, _maxInteger);
 
             return new[] { new List<int> { randomNumber } };
+        }
+
+        private static IEnumerable<int>[] GetCollectionOfIntegers()
+        {
+            List<int> numbers = [];
+            var countOfNumbersToAdd = _random.Next(2, 100);
+
+            for (int i = 0; i < countOfNumbersToAdd; i++)
+            {
+                numbers.Add(GetRandomInteger(_minInteger, _maxInteger));
+            }
+
+            return [
+                numbers,
+                ];
+        }
+
+        private static int GetRandomInteger(int minValue, int maxValue)
+        {
+            return _random.Next(minValue, maxValue);
         }
     }
 }
